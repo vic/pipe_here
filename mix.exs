97b41b1c20168436abb3029b3e81b030c5f17024
Victor Borja <vborja@apache.org>
2017-01-11 10:02:06 -0600

    v1.0.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PipeHere.Mixfile do
 
   def project do
     [app: :pipe_here,
-     version: "1.0.0",
+     version: "1.0.1",
      elixir: "~> 1.2",
      description: description(),
      package: package(),
